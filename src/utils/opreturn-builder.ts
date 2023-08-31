93f6ec5b6c99c8ec52cda52ec45f1e026e061811
Vincent Tse <river_run@126.com>
2023-09-01 03:24:02 +0800

    ok

---
+++ src/utils/opreturn-builder.ts
@@ -0,0 +1,15 @@
+type MetaidOpreturn = [
+  'mvc', // chain flag
+  string, // public key of node
+  string, // `${parentChainFlag(optional)}:${parentTxid}`
+  'metaid',
+  string, // protocol name
+  string, // stringify json body
+  '0', // isEncrypted
+  string, // version
+]
+export default function buildOpreturn({}) {
+  const opreturn = []
+
+  return opreturn
+}
