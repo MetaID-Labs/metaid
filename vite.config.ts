e1df9d8e583a24998adae0e6b1c7771343e55979
JasonYang <yzsenmonster@gmail.com>
2023-10-12 14:12:10 +0800

    ingore

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
 			"@": "/src",
 		},
 	},
-  plugins: [
+	plugins: [
 		dts({
 			insertTypesEntry: true,
 		}),
@@ -23,7 +23,7 @@ export default defineConfig({
 			entry: resolve(__dirname, "src/index.ts"),
 			name: "Metaid",
 			fileName: "metaid",
-			formats: ["es", "cjs", "umd", "iife"],
+			// formats: ["es", "cjs", "umd", "iife"],
 		},
 	},
 });
