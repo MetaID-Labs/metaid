211c3a29e5b20b3e3b4fba2f61e24885715303da
JasonYang <yzsenmonster@gmail.com>
2023-10-18 10:34:03 +0800

    add buzz like

---
+++ src/metaid-entities/like.entity.ts
@@ -0,0 +1,22 @@
+const buzzSchema = {
+	name: "like",
+	nodeName: "PayLike",
+	versions: [
+		{
+			version: 1,
+			id: "2ae43eeb26d9",
+			body: [
+				{
+					name: "likeTo",
+					type: "string",
+				},
+				{
+					name: "isLike",
+					type: "string",
+				},
+			],
+		},
+	],
+};
+
+export default buzzSchema;
