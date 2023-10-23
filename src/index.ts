7c451093049bb119d91d1101f2ed5f4f4eca6748
Vincent Tse <river_run@126.com>
2023-10-23 19:43:57 +0800

    🚀 VERSION: 0.1.1

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ import { use } from '@/factories/use.js'
 import { connect } from '@/factories/connect.js'
 import { LocalWallet } from '@/wallets/local.js'
 import { MetaletWallet } from './wallets/metalet.js'
+import type { EntitySchema } from './metaid-entities/entity.js'
 
 export { define, use, connect, LocalWallet, MetaletWallet }
+export type { EntitySchema }
