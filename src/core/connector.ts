247ead1d8c02d75a8dfffef9b34b7950032e98fb
JasonYang <yzsenmonster@gmail.com>
2023-12-15 10:01:26 +0800

    WIP

--- src/core/connector.ts
+++ src/core/connector.ts
@@ -4,81 +4,75 @@ import { TxComposer } from "meta-contract";
 import { getMetaid } from "@/api.ts";
 
 export class Connector {
-  private _isConnected: boolean;
-  private wallet: MetaIDConnectWallet;
-  public metaid: string | undefined;
+	private _isConnected: boolean;
+	private wallet: MetaIDConnectWallet;
+	public metaid: string | undefined;
 
-  private constructor(wallet: MetaIDConnectWallet) {
-    this._isConnected = true;
+	private constructor(wallet: MetaIDConnectWallet) {
+		this._isConnected = true;
 
-    this.wallet = wallet;
-  }
+		this.wallet = wallet;
+	}
 
-  get address() {
-    return this.wallet.address;
-  }
+	get address() {
+		return this.wallet.address;
+	}
 
-  get xpub() {
-    return this.wallet.xpub;
-  }
+	get xpub() {
+		return this.wallet.xpub;
+	}
+	// todo: add blockchain parameter
+	public static async create(wallet: MetaIDConnectWallet) {
+		const connector = new Connector(wallet);
+		console.log({ wallet });
 
-  public static async create(wallet: MetaIDConnectWallet) {
-    const connector = new Connector(wallet);
-    console.log({ wallet });
+		// ask api for metaid
+		connector.metaid =
+			(await getMetaid({
+				address: wallet.address,
+			})) || undefined;
 
-    // ask api for metaid
-    connector.metaid =
-      (await getMetaid({
-        address: wallet.address,
-      })) || undefined;
+		return connector;
+	}
 
-    return connector;
-  }
+	// metaid
+	hasMetaid() {
+		return !!this.metaid;
+	}
 
-  // metaid
-  hasMetaid() {
-    return !!this.metaid;
-  }
+	use(entitySymbol: string) {
+		return use(entitySymbol, { connector: this });
+	}
 
-  use(entitySymbol: string) {
-    return use(entitySymbol, { connector: this });
-  }
+	isConnected() {
+		return this._isConnected;
+	}
 
-  isConnected() {
-    return this._isConnected;
-  }
+	disconnect() {
+		this._isConnected = false;
+	}
 
-  disconnect() {
-    this._isConnected = false;
-  }
+	/**
+	 * wallet delegation
+	 * signInput / send / broadcast
+	 */
+	signInput({ txComposer, inputIndex }: { txComposer: TxComposer; inputIndex: number }) {
+		return this.wallet.signInput({ txComposer, inputIndex });
+	}
 
-  /**
-   * wallet delegation
-   * signInput / send / broadcast
-   */
-  signInput({
-    txComposer,
-    inputIndex,
-  }: {
-    txComposer: TxComposer;
-    inputIndex: number;
-  }) {
-    return this.wallet.signInput({ txComposer, inputIndex });
-  }
+	send(toAddress: string, amount: number) {
+		return this.wallet.send(toAddress, amount);
+	}
 
-  send(toAddress: string, amount: number) {
-    return this.wallet.send(toAddress, amount);
-  }
+	broadcast(txComposer: TxComposer) {
+		return this.wallet.broadcast(txComposer);
+	}
 
-  broadcast(txComposer: TxComposer) {
-    return this.wallet.broadcast(txComposer);
-  }
+	getPublicKey(path?: string) {
+		return this.wallet.getPublicKey(path);
+	}
 
-  getPublicKey(path?: string) {
-    return this.wallet.getPublicKey(path);
-  }
-
-  getAddress(path?: string) {
-    return this.wallet.getAddress(path);
-  }
+	getAddress(path?: string) {
+		return this.wallet.getAddress(path);
+	}
 }
