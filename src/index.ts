4055bf23118081ffb5e1812551f130add408c6ce
jasonyang <yzsenmonster@gmail.com>
2024-04-07 11:00:47 +0800

    add type export starts with I

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,15 @@ import { useMvc, useBtc } from '@/factories/use.js'
 import { mvcConnect, btcConnect } from '@/factories/connect.js'
 import { LocalWallet } from '@/wallets/localwallet/local.js'
 import { MetaletWalletForMvc } from '@/wallets/metalet/mvc.js'
-import type { EntitySchema } from '@/metaid-entities/entity.js'
 import { MetaletWalletForBtc } from './wallets/metalet/btc.js'
 import { Psbt } from '@/core/entity/btc/bitcoinjs-lib/psbt'
 import { Transaction } from './core/entity/btc/bitcoinjs-lib/transaction.js'
+
+import type { EntitySchema } from '@/metaid-entities/entity.js'
+import type { MetaletWalletForBtc as IMetaletWalletForBtc } from './wallets/metalet/btc.js'
+import type { BtcConnector as IBtcConnector } from './core/connector/btc.js'
+import type { BtcEntity as IBtcEntity } from './core/entity/btc/index.js'
+import type { CreateOptions } from '@/core/entity/btc'
 
 export {
   Psbt,
@@ -20,4 +25,4 @@ export {
   MetaletWalletForMvc,
   MetaletWalletForBtc,
 }
-export type { EntitySchema }
+export type { EntitySchema, IMetaletWalletForBtc, IBtcConnector, IBtcEntity, CreateOptions }
